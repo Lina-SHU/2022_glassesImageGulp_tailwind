3fdd83a6bc1a033ef2b9c7a91a7f319fb97a50ae
Lina-SHU <sakura0477@gmail.com>
2022-08-02 23:47:28 +0800

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#AA0601',
+        'primary-dark': '#650300',
+        'primary-light': '#FBF2F2'
+      }
+    },
+    container: {
+      center: true, // 水平置中
+      padding: '2rem', // 水平間距
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
