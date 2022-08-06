5dc66e2ddb9d25d4ccc26bc7ce284943af794b70
Lina-SHU <sakura0477@gmail.com>
2022-08-06 19:18:02 +0800

    add index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#AA0601',
-        'primary-dark': '#650300',
-        'primary-light': '#FBF2F2'
+        'primary': {
+          'dark': '#650300',
+          DEFAULT: '#AA0601',
+          'light': '#FBF2F2'
+        }
+      },
+      fontSize: {
+        '6xxl': ['64px', '96px']
+      },
+      dropShadow: {
+        'card-shadow': '0px 2px 6px rgba(0, 0, 0, 0.29)'
       }
     },
     container: {
       center: true, // 水平置中
-      padding: '2rem', // 水平間距
+      padding: '12px', // 水平間距
     }
   },
   plugins: [
